698739d94de856ea6107ce5cb30790867bf21f1e
Yolo <vincentrudel@hotmail.com>
2020-09-10 14:27:00 -0400

    no_main test

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -11,22 +11,26 @@ def execute_tests(input, output):
 	for i in sorted(valid_pairs):
 		inp = open("in/"+str(i)+".txt", "r", encoding = "utf8").readlines()
 		exp = open("exp/"+str(i)+".txt", "r", encoding = "utf8").read()
-		passed += int(main(inp) == exp)
+		is_same = main(inp) == exp
+		passed += int(is_same)
+		assert is_same
 	print(str(passed)+"/"+str(n)+" tests passed")
-
+def allo():
+	assert "1"=="2"
+#allo()
 			
-if __name__ == '__main__':
-	input = set()
-	output = set()
+#if __name__ == '__main__':
+input = set()
+output = set()
 
-	for r, d, f in os.walk("in"):
-		for file in f:
-			if '.txt' in file:
-				input.add(file.replace(".txt", ""))
+for r, d, f in os.walk("in"):
+	for file in f:
+		if '.txt' in file:
+			input.add(file.replace(".txt", ""))
 
-	for r, d, f in os.walk("exp"):
-		for file in f:
-			if '.txt' in file:
-				output.add(file.replace(".txt", ""))
+for r, d, f in os.walk("exp"):
+	for file in f:
+		if '.txt' in file:
+			output.add(file.replace(".txt", ""))
 
-	execute_tests(input, output)
+execute_tests(input, output)
