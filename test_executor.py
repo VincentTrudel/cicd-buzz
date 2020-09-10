b98ea7e2639cc8de7567e8fe57cecf7fbd697923
Yolo <vincentrudel@hotmail.com>
2020-09-10 15:47:19 -0400

    cleaned

--- test_executor.py
+++ test_executor.py
@@ -1,35 +1,37 @@
 import os
 from problem import main
 import unittest
-def execute_tests():
 
-	input = set()
-	output = set()
-	
-	for r, d, f in os.walk("in"):
-		for file in f:
-			if '.txt' in file:
-				input.add(file.replace(".txt", ""))
-	
-	for r, d, f in os.walk("exp"):
-		for file in f:
-			if '.txt' in file:
-				output.add(file.replace(".txt", ""))
 
-	valid_pairs = input.intersection(output)
-	n = len(valid_pairs)
-	passed = 0
-	for i in sorted(valid_pairs):
-		inp = open("in/"+str(i)+".txt", "r", encoding = "utf8").readlines()
-		exp = open("exp/"+str(i)+".txt", "r", encoding = "utf8").read()
-		is_same = main(inp) == exp
-		passed += int(is_same)
-		try:
-			assert is_same
-		except AssertionError:
-			print(i, "failed")
-			pass
-	assert 1==2
-	print(str(passed)+"/"+str(n)+" tests passed")
+input = set()
+output = set()
 
-execute_tests()
+for r, d, f in os.walk("in"):
+	for file in f:
+		if '.txt' in file:
+			input.add(file.replace(".txt", ""))
+
+for r, d, f in os.walk("exp"):
+	for file in f:
+		if '.txt' in file:
+			output.add(file.replace(".txt", ""))
+
+valid_pairs = input.intersection(output)
+success = {}
+for i in sorted(valid_pairs):
+	inp = open("in/"+i+".txt", "r", encoding = "utf8").readlines()
+	exp = open("exp/"+i+".txt", "r", encoding = "utf8").read()
+	is_same = main(inp) == exp
+	success[int(i)]=is_same
+
+def test_0():
+	assert success[0]
+def test_1():
+	assert success[1]
+def test_2():
+	assert success[2]
+def test_10():
+	assert success[10]
+def test_15():
+	assert success[15]
+
